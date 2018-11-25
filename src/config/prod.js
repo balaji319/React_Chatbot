0c1a47ff4a21ad6007f6e62e215d732ab6632d24
sapna <sapnapastapure@gmail.com>
2018-11-25 15:11:36 +0530

    initial commit

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
     googleProjectId : process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionId : process.env.DIALOGFLOW_SESSION_ID,
-    dialgFlowSessionLanaugeCode : process.env.DIALOGFLOW_SESSION_LANAUGE_CODE
+    dialgFlowSessionLanaugeCode : process.env.DIALOGFLOW_SESSION_LANAUGE_CODE,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey : process.env.GOOGLE_PRIVATE_KEY
 }
